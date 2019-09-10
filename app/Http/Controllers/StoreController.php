df04019bc696a63ef5c58c12b12be068875b4ece
mafifi <mafifi@gocardi.com>
2019-09-10 12:43:25 +0200

    get the exception

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -24,7 +24,7 @@ class StoreController extends Controller
         $request = Request::create($this->_link . '/getAllItemsApi', 'GET');
         $res = app()->handle($request);
         $allItems = json_decode($res->getContent());
-        dd($allItems);
+        dd($res);
         return view('index', ['allItems' => $allItems, 'link' => $this->_link]);
     }
 
