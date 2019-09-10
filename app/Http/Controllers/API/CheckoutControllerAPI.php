bab958974d6f9383441e408ab0627b2c63f2c864
mafifi <mafifi@gocardi.com>
2019-09-10 16:35:50 +0200

    get the exception

--- app/Http/Controllers/API/CheckoutControllerAPI.php
+++ app/Http/Controllers/API/CheckoutControllerAPI.php
@@ -14,7 +14,6 @@ class CheckoutControllerAPI extends Controller
     //make the checkout action
     public function checkout(Request $request)
     {
-        return response()->json(['status' => $request->all()['telephone']]);
         try {
 
             //find customer based on id let's say 1
@@ -26,10 +25,11 @@ class CheckoutControllerAPI extends Controller
                 return response()->json(['status' => false, 'msg' => 'error happened', 'reason' => 'credit']);
             }
             //if credit enough then fire the event
+            Order::create(['customer_id' => 1, 'total' => $request->all()->totalPrice, 'address' => $request->all()['address'], 'telephone' => $request->all()['telephone']]);
             event(new Checkout($request->all()));
             return response()->json(['status' => true, 'msg' => 'done successfully']);
         } catch (\Exception $exception) {
-            return response()->json(['status' => false, 'msg' => 'error happened', 'reason' => 'error']);
+            return response()->json(['status' => false, 'msg' => $exception->getMessage(), 'reason' => 'error']);
         }
     }
 }
