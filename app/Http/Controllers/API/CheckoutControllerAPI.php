75d88fdeee91b58c9c986011a8ac58d763b4cd06
mafifi <mafifi@gocardi.com>
2019-09-10 13:54:16 +0200

    get the exception

--- app/Http/Controllers/API/CheckoutControllerAPI.php
+++ app/Http/Controllers/API/CheckoutControllerAPI.php
@@ -16,14 +16,13 @@ class CheckoutControllerAPI extends Controller
         try {
             //find customer based on id let's say 1
             $userCredit = Customer::find(1)->store_credit;
+
             //get cache credit and then compare with user credit
-            return response()->json(['status' => Cache::get('cart'), 'msg' => 'done successfully']);
             if (Cache::get('cart')->totalPrice > $userCredit) {
                 return response()->json(['status' => false, 'msg' => 'error happened', 'reason' => 'credit']);
             }
             //if credit enough then fire the event
             event(new Checkout($request->all()));
-
             return response()->json(['status' => true, 'msg' => 'done successfully']);
         } catch (\Exception $exception) {
             return response()->json(['status' => false, 'msg' => 'error happened', 'reason' => 'error']);
